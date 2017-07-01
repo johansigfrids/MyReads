875ef584e577567a23dd4c63372c55cb5ced41ce
Johan Sigfrids <johan.sigfrids@gmail.com>
2017-07-01 18:52:28 +0300

    refactor: split out bookshelf code into separate component

---
+++ src/Bookshelf.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Book from './Book';
+
+function Bookshelf(props) {
+  return (
+    <div className="bookshelf">
+      <h2 className="bookshelf-title">{props.title}</h2>
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {props.books.map(book => (
+            <li>
+              <Book key={book.id} book={book} />
+            </li>
+          ))}
+        </ol>
+      </div>
+    </div>
+  );
+}
+
+export default Bookshelf;
