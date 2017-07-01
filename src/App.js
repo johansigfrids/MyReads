a90d1602f3f741934b214934b85ae85094164fa8
Johan Sigfrids <johan.sigfrids@gmail.com>
2017-07-01 19:03:58 +0300

    feat: Show books from server.
    Use BooksAPI to fetch the books from the server in App.componentDidMount, and then pass them down to MyBooks as props.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react';
-import * as BooksAPI from './BooksAPI';
 import './App.css';
+import * as BooksAPI from './BooksAPI';
 import {Route} from 'react-router-dom';
 import MyBooks from './MyBooks';
 import Search from './Search';
 
 class BooksApp extends React.Component {
+  state = {
+    currentlyReading: [],
+    wantToRead: [],
+    read: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      const currentlyReading = books.filter(b => b.shelf === 'currentlyReading');
+      const wantToRead = books.filter(b => b.shelf === 'wantToRead');
+      const read = books.filter(b => b.shelf === 'read');
+      this.setState({ currentlyReading, wantToRead, read});
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={MyBooks} />
+        <Route exact path="/" render={() => (
+          <MyBooks 
+            currentlyReading={this.state.currentlyReading}
+            wantToRead={this.state.wantToRead}
+            read={this.state.read}
+          />
+        )} />
         <Route path="/search" component={Search} />
       </div>
     )
